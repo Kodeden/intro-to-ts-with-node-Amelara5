b0ae3a0f39b3c61665d4b998dd5de2f90c95dd68
Andres Melara <andres.melara.t@gmail.com>
2024-04-06 02:41:51 -0400

    Nesting interfaces. Added 'Address' to 'Person'

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { describeCar } from "./challenge.js";
 
+interface Address {
+  street: string;
+  city: string;
+  state: string;
+  zip: number;
+}
+
 interface Person {
+  readonly id: number;
   firstName: string;
   lastName: string;
   age: number;
+
+  address: Address;
+
+  hobbies?: string[];
 }
 
 export function greet(personName: Person): string {
@@ -11,22 +23,29 @@ export function greet(personName: Person): string {
 }
 
 const citizen = {
+  id: 1,
   firstName: "Andres",
   lastName: "Melara",
   age: 27,
+  address: {
+    street: "571 Fake Ave",
+    city: "Falseville",
+    state: "DC",
+    zip: 20006,
+  },
 };
 
 console.log(greet(citizen));
 
-const exampleModel = {
-  make: "Toyota",
-  model: "Corolla",
-  year: 2024,
-  color: "black",
-  isElectric: false,
-};
+// const exampleModel = {
+//   make: "Toyota",
+//   model: "Corolla",
+//   year: 2024,
+//   color: "black",
+//   isElectric: false,
+// };
 
-const exampleModel2 = { ...exampleModel, isElectric: true, remoteStart: true };
+// const exampleModel2 = { ...exampleModel, isElectric: true, remoteStart: true };
 
-console.log(describeCar(exampleModel));
-console.log(describeCar(exampleModel2));
+// console.log(describeCar(exampleModel));
+// console.log(describeCar(exampleModel2));
